b287fdb190488d7f4b7ec825a06387e322ebae48
devdossett <github@dossett.dev>
2020-06-18 23:34:44 -0500

    Changed text on home page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>beans.js</code> and save to reload.
+          Hello World. This is a react app.
         </p>
         <a
           className="App-link"
